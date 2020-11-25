367403a7a22d88b9b8df4db5a29ec9ffffef7cbd
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-11-25 02:35:44 -0600

    Modificando index solo para test

--- dist/index.php
+++ dist/index.php
@@ -158,7 +158,7 @@ if (!isset($_SESSION['usuario_admin'])) {
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                                    <div class="card-body">Tarjeta Roja</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
