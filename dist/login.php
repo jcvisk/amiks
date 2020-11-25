af63035063966585c70170d0b2fa25911a063cef
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-11-25 04:49:04 -0600

    link para generar administrador

--- dist/login.php
+++ dist/login.php
@@ -51,7 +51,7 @@ require_once '../src/core/helpers.php';
                                     </form>
                                 </div>
                                 <div class="card-footer text-center">
-                                    <!-- <div class="small">Texto de jemplo</div> -->
+                                     <div class="small"><a href="../src/core/create_admin.php">Crear Admin</a></div> 
                                 </div>
                             </div>
                         </div>
