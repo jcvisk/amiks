da922e71717e5f65cb8f6a6676c72dd5422d661b
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-11-26 15:17:29 -0600

    modificando select para que solo me obtenga los datos en status 1

--- src/core/helpers/helpers_products.php
+++ src/core/helpers/helpers_products.php
@@ -2,7 +2,7 @@
 
 //Devuelve todos los registros de una tabla
 function getAllRecords( $conexion, $tabla ) {
-    $sql = "SELECT * FROM $tabla ";
+    $sql = "SELECT * FROM $tabla WHERE status = 1";
     $datos = mysqli_query( $conexion, $sql );
 
     $resultado = array();
