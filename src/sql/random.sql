fcd3aa0b755bc217f039a4229b489c5241361588
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-11-26 15:17:46 -0600

    nuevos datos de busqueda, modificacion etc

--- src/sql/random.sql
+++ src/sql/random.sql
@@ -1,2 +1,11 @@
 drop database amiks;
 create database amiks;
+
+--Ver la estrucctuta de una tabla
+SHOW COLUMNS from productos;
+
+--Cambiar el nombre de una columna
+ALTER TABLE productos CHANGE `precio` `precioBase` DOUBLE;
+
+--Agregar una columna despues de otra
+ALTER TABLE productos ADD precioVenta DOUBLE AFTER precioBase;
