ac1ffc093c8060d0e3abcad9cf5ef7b03eb45ac3
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-12-02 08:33:48 -0600

    eliminando paginas que no se usan y regresando el form al index

--- dist/index.php
+++ dist/index.php
@@ -25,6 +25,10 @@ if (!isset($_SESSION['usuario_admin'])) {
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a class="navbar-brand" href="index.html">Start Bootstrap</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search -->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+             
+            </form>
             <!-- Navbar-->
             <ul class="navbar-nav ml-auto ml-md-0">
                 <li class="nav-item dropdown">
