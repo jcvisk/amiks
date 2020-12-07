d4be7d9c54e4b128d1b57bba1f602c1ea467cf5b
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-12-06 21:03:38 -0600

    cambiando el tipo de datos en caso de que el post venga vacio de '' a null

--- src/core/edit_client.php
+++ src/core/edit_client.php
@@ -3,15 +3,15 @@ if ( isset( $_POST ) ) {
     require_once 'conexion.php';
     require_once 'helpers.php';
     $id = isset( $_POST['id'] ) ? (INT)mysqli_real_escape_string( $conexion, $_POST['id'] ) : '' ;
-    $nombreEmpresa = isset( $_POST['nombre_empresa'] ) ? mysqli_real_escape_string( $conexion, $_POST['nombre_empresa'] ) : '' ;
-    $propietario = isset( $_POST['propietario'] ) ? mysqli_real_escape_string( $conexion, $_POST['propietario'] ) : '' ;
-    $ubicacion = isset( $_POST['ubicacion'] ) ? mysqli_real_escape_string( $conexion, $_POST['ubicacion'] ) : '' ;
-    $telefono = isset( $_POST['telefono'] ) ? mysqli_real_escape_string( $conexion, $_POST['telefono'] ) : '' ;
-    $celular = isset( $_POST['celular'] ) ? mysqli_real_escape_string( $conexion, $_POST['celular'] ) : '' ;
+    $nombreEmpresa = isset( $_POST['nombre_empresa'] ) ? mysqli_real_escape_string( $conexion, $_POST['nombre_empresa'] ) : null ;
+    $propietario = isset( $_POST['propietario'] ) ? mysqli_real_escape_string( $conexion, $_POST['propietario'] ) : null ;
+    $ubicacion = isset( $_POST['ubicacion'] ) ? mysqli_real_escape_string( $conexion, $_POST['ubicacion'] ) : null ;
+    $telefono = isset( $_POST['telefono'] ) ? mysqli_real_escape_string( $conexion, $_POST['telefono'] ) : null ;
+    $celular = isset( $_POST['celular'] ) ? mysqli_real_escape_string( $conexion, $_POST['celular'] ) : null ;
     $distribuidor = isset( $_POST['distribuidor'] ) ? (INT) mysqli_real_escape_string( $conexion, $_POST['distribuidor'] )  : '' ;
-    
+    //echo $id.' '.$nombreEmpresa.' '.$propietario.' '.$ubicacion.' '.$telefono.' '.$celular.' '.$distribuidor; die();
     /*Tabla clientes*/
-    $sqlClientes = "UPDATE clientes SET nombreempresa='$nombreEmpresa', propietario='$propietario', ubicacion='$ubicacion', telefono='$telefono', celular='$celular', iddistribuidor=$distribuidor WHERE id = 7;";
+    $sqlClientes = "UPDATE clientes SET nombreempresa='$nombreEmpresa', propietario='$propietario', ubicacion='$ubicacion', telefono='$telefono', celular='$celular', iddistribuidor=$distribuidor WHERE id = $id;";
     $saveClientes = mysqli_query( $conexion, $sqlClientes );
     if ( $saveClientes ) {
         header( 'Location: ../../dist/register_client.php' );
